903f7b2b51c501459737084a3636a7a9d1b33e6a
thejsj <jorge.silva@thejsj.com>
2017-02-16 13:40:48 -0800

    Fix content-type header

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 var server = http.createServer((req, res) => {
   console.log('Request coming in...')
-  res.writeHead(200, {'Content-Type': 'appplication/json'})
+  res.writeHead(200, {'Content-Type': 'application/json'})
   res.end(JSON.stringify({
     hello: Math.random(),
     process: process.env
